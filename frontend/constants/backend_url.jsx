0ba11c2dc6b63f2c5babe73fa12077a0a16c5a81
manav <padwalmanav03@gmail.com>
2025-12-29 18:51:42 +0530

    backend url updated to latest one

--- frontend/constants/backend_url.jsx
+++ frontend/constants/backend_url.jsx
@@ -1,4 +1,4 @@
-// const BACKEND_URL = "";
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://event-intelligence-system-0.onrender.com";
+// const BACKEND_URL = "http://localhost:8000";
 
 export default BACKEND_URL;
