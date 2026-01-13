4f167f25e9e871f5c0c4a02de057f04f8a6593af
manav <padwalmanav03@gmail.com>
2026-01-13 18:05:07 +0530

    routing bug fixed when tried to view insights on login

--- frontend/components/ProtectedEventRoute.jsx
+++ frontend/components/ProtectedEventRoute.jsx
@@ -2,7 +2,9 @@ import { Navigate, useParams } from "react-router-dom";
 
 const ProtectedEventRoute = ({children}) => {
     const params = useParams();
-    return params.id == '695cc46eadcc1eb4056188b2' ? children : <Navigate to='/events' replace/>
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === "true"
+
+    return params.id == '695cc46eadcc1eb4056188b2' || isLoggedIn ? children : <Navigate to='/events' replace/>
 }
 
 export default ProtectedEventRoute;
